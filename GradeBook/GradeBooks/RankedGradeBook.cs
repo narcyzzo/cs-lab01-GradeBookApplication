b6bdd07a1f410d9fb243d1e3b36d4c250b82d4ae
narcyzzo <kubavip002@gmail.com>
2022-04-03 13:58:10 +0200

    Overrided CalculateStudentStatistics method

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,38 +13,49 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
         public override char GetLetterGrade(double averageGrade)
         {
-            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
+            var threshold = (int) Math.Ceiling(Students.Count * 0.2);
 
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
-            if (grades[threshold - 1] <= averageGrade)  
+            if (grades[threshold - 1] <= averageGrade)
                 return 'A';
-            if (grades[(threshold*2)-1] <= averageGrade)
-                return 'B';  
-            if (grades[(threshold*3)-1] <= averageGrade)
+            if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            if (grades[(threshold * 3) - 1] <= averageGrade)
                 return 'C';
-            if (grades[(threshold*4)-1] <= averageGrade)
+            if (grades[(threshold * 4) - 1] <= averageGrade)
                 return 'D';
             return 'F';
 
 
         }
-        
-        
+
+
         public override void CalculateStatistics()
         {
-            if (Students.Count <5)
+            if (Students.Count < 5)
             {
                 Console.WriteLine("Ranked grading requires at least 5 students");
                 return;
             }
+
             base.CalculateStatistics();
         }
-        
-        
-        
-        
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+
+
+        }
     }
 }
